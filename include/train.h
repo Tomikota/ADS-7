b0e28fa80ca1f43cf8aa3266311eb7d8c08348fe
Tomikota <savinov.ze@yandex.ru>
2026-05-31 23:10:54 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,22 +1,37 @@
-// Copyright 2022 NNTU-CS
-#ifndef TRAIN_H
-#define TRAIN_H
+// Copyright 2025
+
+#ifndef INCLUDE_TRAIN_H_
+#define INCLUDE_TRAIN_H_
 
 class Train {
  private:
-   struct Car {
-      bool light;
-      Car *next;
-      Car *prev;
-   };
-   int countOp;
-   Car *first;
+    struct Car {
+        bool light;
+
+        Car* next;
+        Car* prev;
+
+        Car() {
+            light = false;
+            next = nullptr;
+            prev = nullptr;
+        }
+    };
+
+    Car* first;
+
+    int countOp;
+
  public:
-   Train(); 
-   ~Train();
-   void addCar(bool light);
-   int  getLength();
-   int  getOpCount();
+    Train();
+
+    ~Train();
+
+    void addCar(bool light);
+
+    int getLength();
+
+    int getOpCount();
 };
 
-#endif
+#endif  // INCLUDE_TRAIN_H_
