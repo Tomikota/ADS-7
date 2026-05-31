f913eda5a420eadd632505bf4d6a067195d703a8
Tomikota <savinov.ze@yandex.ru>
2026-05-31 23:11:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,62 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2025
+
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <random>
-#include <sys/stat.h>
+
 #include "train.h"
 
-#if defined(_WIN32)
-#include <direct.h>
-#endif
-
-void createResultDirectory() {
-#if defined(_WIN32)
-    _mkdir("result");
-#else
-    mkdir("result", 0777);
-#endif
-}
-
 int main() {
-    createResultDirectory();
-    std::ofstream outFile("result/data.csv");
-    if (!outFile.is_open()) {
-        return 1;
-    }
-    
-    outFile << "N,AllOff,AllOn,Random\n";
-    
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 1);
-    
-    for (int N = 2; N <= 100; ++N) {
-        Train trainOff;
-        for (int i = 0; i < N; ++i) {
-            trainOff.addCar(false);
-        }
-        trainOff.getLength();
-        int opsOff = trainOff.getOpCount();
-        
-        Train trainOn;
-        for (int i = 0; i < N; ++i) {
-            trainOn.addCar(true);
-        }
-        trainOn.getLength();
-        int opsOn = trainOn.getOpCount();
-        
-        int runs = 15;
-        double opsRandSum = 0;
-        for (int r = 0; r < runs; ++r) {
-            Train trainRand;
-            for (int i = 0; i < N; ++i) {
-                trainRand.addCar(dis(gen) == 1);
+    std::srand(static_cast<unsigned>(time(nullptr)));
+
+    std::cout << "n false true random\n";
+
+    for (int n = 2; n <= 100; n++) {
+        int opFalse;
+        int opTrue;
+        int opRandom;
+
+        {
+            Train train;
+
+            for (int i = 0; i < n; i++) {
+                train.addCar(false);
             }
-            trainRand.getLength();
-            opsRandSum += trainRand.getOpCount();
+
+            train.getLength();
+
+            opFalse = train.getOpCount();
         }
-        double opsRandAvg = opsRandSum / runs;
-        
-        outFile << N << "," << opsOff << "," << opsOn << "," << opsRandAvg << "\n";
+
+        {
+            Train train;
+
+            for (int i = 0; i < n; i++) {
+                train.addCar(true);
+            }
+
+            train.getLength();
+
+            opTrue = train.getOpCount();
+        }
+
+        {
+            Train train;
+
+            for (int i = 0; i < n; i++) {
+                train.addCar(std::rand() % 2);
+            }
+
+            train.getLength();
+
+            opRandom = train.getOpCount();
+        }
+
+        std::cout << n << " "
+                  << opFalse << " "
+                  << opTrue << " "
+                  << opRandom << std::endl;
     }
-    
-    outFile.close();
+
     return 0;
 }
